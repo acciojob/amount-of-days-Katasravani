6fcc33da720abf07c09b2aa0f827d78319c6229c
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-24 11:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+function daysOfAYear() {
+    // Function to check if a year is a leap year
+    function isLeapYear(year) {
+        return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+    }
+
+    // Get the year from user input
+    const year = parseInt(prompt("Enter a year:"));
+
+    // Validate if the input is a valid year
+    if (isNaN(year) || year < 1 || year > 9999) {
+        alert("Please enter a valid year.");
+        return;
+    }
+
+    // Number of days in a non-leap year
+    const daysInNonLeapYear = 365;
+
+    // Number of days in a leap year
+    const daysInLeapYear = 366;
+
+    // Determine the number of days based on whether it's a leap year
+    const result = isLeapYear(year) ? daysInLeapYear : daysInNonLeapYear;
+
+    // Display the result
+    alert(${result});
+}
+
+// Call the function to get user input and display the result
+daysOfAYear();

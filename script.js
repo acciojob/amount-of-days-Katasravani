6d46bd2512ac8c750a5525b0350935994d1a2531
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-11-24 11:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function daysOfAYear() {
     const result = isLeapYear(year) ? daysInLeapYear : daysInNonLeapYear;
 
     // Display the result
-    alert(`The number of days in ${year} is: ${result}`);
+    alert(result);
 }
 
 // Call the function to get user input and display the result
